e55855bcd059ac1cce65dd3d31fa04e69fd866db
Kevin Chan <kevinchan128@msn.com>
2021-02-16 22:49:38 -0800

    Commit 2
    removed client folder and .js file

--- server/server.js
+++ server/server.js
@@ -1,56 +1,78 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  //middleware gets applied to all handlers
-  //gets called before the callback on each handler
-  console.log('Request incoming');
-  console.log(req.originalUrl);
-  console.log(req.headers['user-agent']);
-  next();
-});
+// app.use((req, res, next) => {
+//   //middleware gets applied to all handlers
+//   //gets called before the callback on each handler
+//   console.log('Request incoming');
+//   console.log(req.originalUrl);
+//   console.log(req.headers['user-agent']);
+//   next();
+// });
 
 //parse body to json; a string of information
 app.use(express.json());
 
 // Todo: Make endpoints
 
+const body = {
+  title: null,
+  description: null,
+  price: null,
+  type: null,
+  uid: 0
+};
+
 //body should have json****
 app.post('/createListing', (req, res) => {
   console.log(req.body);
+  uid++;
+  res.status(200).send('Body received.');
 });
+
 
 //returns all listings
-app.get('/viewListings', (req, res) => {
-  console.log(`Here is the listing:\n${req.body}`);
-});
-
-//returns all listings only by matching type
-app.get('/viewListing?type=<type>', (req, res) => {
-
+app.post('/api/viewListings', (req, res) => {
+  console.log(req.allListings);
+  res.status(200).send('allListings received.');
 });
 
 //delete listing by uid
-app.post('/deleteListing?id=<id>', (req, res) => {
-
+app.delete('/api/deleteListing', (req, res) => {
+  //req.query.id
 });
 
 //make inquiry(like comment to ask about listing)
-app.post('/makeInquiry', (req, res) => {
+app.post('/api/makeInquiry', (req, res) => {
   //body should have json
-  //keys: message
 });
 
 ////returns all inquiries for a listing
-app.get('/getInquiries?listingId=<listingId>', (req, res) => {
+app.get('/api/getInquiries', (req, res) => {
   
 });
+
+//default route
+app.get('/', (req, res) => {
+  res.status(404).send('Hello World!!!');
+  console.log(res.statusCode);
+});
+
+//for random endpoint entered
+app.get('*', (req, res) => {
+  res.status(404);
+  console.log(res.statusCode);
+});
+
+// app.post('/listingB', (req, res) => {
+//   console.log(req.response);
+//   console.log('Response received.');
+// });
+
 
 module.exports = app;
 
 if (require.main === module) {
   console.log('Starting app');
-  app.listen(4000);
+  app.listen(3000);
 }
-
-
